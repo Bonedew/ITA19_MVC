48d507721fd04a8eacf1f7110a80850faee1e47e
Ruuben <ruuben112@gmail.com>
2020-09-09 09:10:01 +0300

    on lisatus main funktsioon programmi käivitamiseks

--- helpers.py
+++ helpers.py
@@ -11,20 +11,25 @@ def lisa_elemendid(elementide_nimekiri):
     global elemendid
     elemendid = elementide_nimekiri
 
-# teeme katseandemstik
-katse_elemendid = [
-    {"nimetus": "leib", "hind": 0.80, "kogus": 20},
-    {"nimetus": "piim", "hind": 0.50, "kogus": 15},
-    {"nimetus": "vein", "hind": 5.60, "kogus": 5},
-]
+def main():
+    # teeme katseandemstik
+    katse_elemendid = [
+        {"nimetus": "leib", "hind": 0.80, "kogus": 20},
+        {"nimetus": "piim", "hind": 0.50, "kogus": 15},
+        {"nimetus": "vein", "hind": 5.60, "kogus": 5},
+    ]
 
-# testime elementide lisamist
-lisa_elemendid(katse_elemendid)
-lisa_elemendid(elemendid)
-print(katse_elemendid)
-print("-----------")
+    # testime elementide lisamist
+    lisa_elemendid(katse_elemendid)
+    lisa_elemendid(elemendid)
+    print(katse_elemendid)
+    print("-----------")
 
-# testime üksiku elemendi lisamist
+    # testime üksiku elemendi lisamist
 
-lisa_element("kohupiim", 0.6, 15)
-print(elemendid)
+    lisa_element("kohupiim", 0.6, 15)
+    print(elemendid)
+
+# käivitamine
+if __name__ == "__main__":
+    main()
