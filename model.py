ab13db60447079d198a065d896568bb6ab0ae4e4
Ruuben <ruuben112@gmail.com>
2020-09-14 14:26:24 +0300

    ´d

---
+++ model.py
@@ -0,0 +1,13 @@
+import helpers
+
+
+class Model:
+     def __init__(self, andmed):
+         self.lisa_elemendid(andmed)
+
+    def lisa_elemendid(self, elemendid):
+        helpers.lisa_elemendid(elemendid)
+
+
+    def loe_elemendid(self):
+        helpers.loe_elemendid()
